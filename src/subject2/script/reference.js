ba21f2c448f9ea483bd87e7efce1e7d7b6b1d017
kimjiyoung0527 <143327785+kimjiyoung0527@users.noreply.github.com>
2023-09-05 20:35:15 +0900

    프로젝트 1차 제출

---
+++ src/subject2/script/reference.js
@@ -0,0 +1,9 @@
+const reference = [
+  {
+    author: 'Good Mockups',
+    title: 'Free Duct Tape Strips Mockup PSD',
+    year: '2023',
+    sourceTitle: '',
+    link: 'https://goodmockups.com/free-duct-tape-strips-mockup-psd/',
+  },
+];
---
+++ src/subject3/script/reference.js
@@ -0,0 +1,9 @@
+const reference = [
+  {
+    author: '윤재천',
+    title: '구름 위에 지은 집',
+    year: '2018',
+    sourceTitle: '',
+    link: 'https://product.kyobobook.co.kr/detail/S000001084960',
+  },
+];
